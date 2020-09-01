22f425a944fb5d13904aacdae8b4e96c941657f3
Les Aker <me@lesaker.org>
2020-09-01 00:41:29 +0000

    add influx push

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/akerl/wh31e_metrics
 
 go 1.15
 
-require gopkg.in/mcuadros/go-syslog.v2 v2.3.0
+require (
+	github.com/ghodss/yaml v1.0.0
+	github.com/influxdata/influxdb-client-go/v2 v2.0.1
+	gopkg.in/mcuadros/go-syslog.v2 v2.3.0
+)
--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
 
+	"github.com/ghodss/yaml"
+	"github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api/write"
 	"gopkg.in/mcuadros/go-syslog.v2"
 	"gopkg.in/mcuadros/go-syslog.v2/format"
 )
+
+type config struct {
+	InfluxURL    string         `json:"influx_url"`
+	InfluxToken  string         `json:"influx_token"`
+	InfluxOrg    string         `json:"influx_org"`
+	InfluxBucket string         `json:"influx_bucket"`
+	SyslogHost   string         `json:"syslog_host"`
+	SyslogPort   int            `json:"syslog_port"`
+	SensorNames  map[int]string `json:"sensor_names"`
+}
 
 type message struct {
 	TimeStr      string  `json:"time"`
@@ -17,42 +34,127 @@ type message struct {
 	Humidity     int     `json:"humidity"`
 }
 
-func handle(log format.LogParts) error {
+func (m message) ChannelStr() string {
+	return string(m.Channel)
+}
+
+func (m message) IDStr() string {
+	return string(m.ID)
+}
+
+func (m message) Name(conf config) string {
+	name := conf.SensorNames[m.Channel]
+	if name == "" {
+		name = string(m.Channel)
+	}
+	return name
+}
+
+func (m message) TemperatureF() float64 {
+	return m.TemperatureC*9/5 + 32
+}
+
+func (m message) Time() (time.Time, error) {
+	return time.Parse("2006-01-02 15:04:05", m.TimeStr)
+}
+
+func (m message) ToPoint(conf config) (*write.Point, error) {
+	t, err := m.Time()
+	if err != nil {
+		return nil, err
+	}
+	p := influxdb2.NewPoint(
+		"wh31e",
+		map[string]string{
+			"channel": m.ChannelStr(),
+			"id":      m.IDStr(),
+			"name":    m.Name(conf),
+		},
+		map[string]interface{}{
+			"temperature_f": m.TemperatureF(),
+			"temperature_c": m.TemperatureC,
+			"humidity":      m.Humidity,
+			"battery":       m.BatteryInt,
+		},
+		t,
+	)
+	return p, nil
+}
+
+func parse(log format.LogParts) (message, error) {
+	var m message
+
 	data, ok := log["message"].(string)
 	if !ok {
-		return fmt.Errorf("failed to coerce message to byte slice")
+		return m, fmt.Errorf("failed to coerce message to byte slice")
 	}
 
-	var m message
 	err := json.Unmarshal([]byte(data), &m)
-	if err != nil {
-		return err
-	}
+	return m, err
+}
 
-	fmt.Printf("%+v\n", m)
+func loop(conf config, channel syslog.LogPartsChannel) error {
+	client := influxdb2.NewClient(conf.InfluxURL, conf.InfluxToken)
+	defer client.Close()
+	writeAPI := client.WriteAPIBlocking(conf.InfluxOrg, conf.InfluxBucket)
+
+	for log := range channel {
+		m, err := parse(log)
+		if err != nil {
+			return err
+		}
+		p, err := m.ToPoint(conf)
+		if err != nil {
+			return err
+		}
+		err = writeAPI.WritePoint(context.Background(), p)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
-func loop(channel syslog.LogPartsChannel) {
-	for msg := range channel {
-		if err := handle(msg); err != nil {
-			panic(err)
-		}
-	}
-}
-
-func main() {
-	channel := make(syslog.LogPartsChannel)
+func launchSyslogServer(conf config, channel syslog.LogPartsChannel) error {
 	handler := syslog.NewChannelHandler(channel)
-	go loop(channel)
-
 	server := syslog.NewServer()
 	server.SetFormat(syslog.RFC5424)
 	server.SetHandler(handler)
-	server.ListenUDP("127.0.0.1:1514")
+	host := fmt.Sprintf("%s:%d", conf.SyslogHost, conf.SyslogPort)
+	server.ListenUDP(host)
+	return server.Boot()
+}
 
-	if err := server.Boot(); err != nil {
-		panic(err)
+func loadConfig(file string) (config, error) {
+	var c config
+
+	contents, err := ioutil.ReadFile(file)
+	if err != nil {
+		return c, err
 	}
-	server.Wait()
+
+	err = yaml.Unmarshal(contents, &c)
+	return c, err
+}
+
+func die(err error) {
+	fmt.Println(err)
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		die(fmt.Errorf("config file must be given as argument"))
+	}
+	c, err := loadConfig(os.Args[1])
+	if err != nil {
+		die(err)
+	}
+	channel := make(syslog.LogPartsChannel)
+	if err := launchSyslogServer(c, channel); err != nil {
+		die(err)
+	}
+	if err := loop(c, channel); err != nil {
+		die(err)
+	}
 }
